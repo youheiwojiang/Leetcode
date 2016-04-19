2a6c5ecb8a7310090885a7f6004d8a2b70b4d77b
Jiaan <jwang682@wisc.edu>
2016-04-19 16:12:30 -0500

    add close 3 sum

--- lc_16.java
+++ lc_16.java
@@ -1,36 +1,25 @@
 public class Solution {
-    public List<List<Integer>> fourSum(int[] nums, int target) {
-      List<List<Integer>>res = new ArrayList<List<Integer>>();
-      Arrays.sort(nums);
-      int pred1 = 0,pred2 = 0;
-      for(int i = 0; i<nums.length-3;i++){
-          if(i>0 && nums[i] == pred1)
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int res = nums[0]+nums[1]+nums[2],pred = 0;
+        for(int i = 0 ; i< nums.length-2;i++){
+            // under this assumption unique sol the next 3 lines are useless
+            /*
+            if(i > 0 && nums[i] == pred)
                 continue;
-          // remember to change values to the two predecessors(pred1,pred2);
-          pred1 = nums[i];
-          for(int j = i+1;j<nums.length-2;j++){
-              if(j>i+1 && nums[j] == pred2)
-                 continue;
-              pred2 = nums[j];
-              int l = j+1,r = nums.length-1;
-              while(l<r){
-                  int sum = nums[i] + nums[j] + nums[l] + nums[r];
-                  if(sum == target){
-                      res.add(Arrays.asList(nums[i],nums[j],nums[l],nums[r]));
-                      while(nums[r] == nums[r-1] && r > l)
-                            r--;
-                      while(nums[l] == nums[l+1] && l < r)
-                            l++;
-                      r--;
-                      l++;
-                  }else if(sum < target){
-                      l++;
-                  }else{
-                      r--;
-                  }
-              }
-          }
-      }
-      return res;
+            pred = nums[i];
+            */
+            int l = i+1,r = nums.length-1;
+            while(l < r){
+                int sum = nums[i] + nums[l] + nums[r];
+                if(sum == target)
+                    return target;
+                if(Math.abs(target - sum) < Math.abs(res - target))
+                        res = sum;
+                if(target > sum)l++;
+                else r--;
+            }
+        }
+        return res;
     }
 }
