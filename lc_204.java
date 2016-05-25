9d57923e40901c8702fe7b3e6a11f51fa44b8b52
Jiaan <jwang682@wisc.edu>
2016-05-25 15:45:01 -0500

    test a new branch

---
+++ lc_204.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public int countPrimes(int n) {
+        boolean check[] = new boolean[n];
+        int count = 0;
+        for(int i = 2; i< n; i++){
+            if(check[i]) continue;
+            count++;
+            for(int j = i; j<n;j+=i)
+                check[j] = true;
+        }
+        return count;
+    }
+}
